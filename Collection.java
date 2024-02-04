58b2ec0934e17bab0c084973a4194dae7c89b0ca
mimsy02 <158780483+mimsy02@users.noreply.github.com>
2024-02-04 02:57:08 -0500

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -8,19 +8,19 @@ public class Collection {
         for (int row = 0; row < collection.length; row++){
             for (int col = 0; col < collection[row].length; col++) {
               
-                    // Planet 1
+                    // Walking
                 if (row == 0 && col == 0){
-                    collection[row][col] = new Collectable(1,1);
+                    collection[row][col] = new Collectable(1,1);    //Animal
                 } else if (row == 0 && col == 1){
-                    collection[row][col] = new Collectable(1,2);
+                    collection[row][col] = new Collectable(1,2);    //Object
                   
-                    // Planet 2
+                    // Yoga
                 } else if (row == 1 && col == 0){
                     collection[row][col] = new Collectable(2,1);
                 } else if (row == 1 && col == 1) {
                     collection[row][col] = new Collectable(2,2);
                   
-                    // Planet 3
+                    // Gym
                 } else if (row == 2 && col == 0) {
                     collection[row][col] = new Collectable(3,1);
                 } else if (row == 2 && col == 1) {
